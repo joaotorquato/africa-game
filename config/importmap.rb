af317aa261cd8f0689e50c157fc4013abc32c187
João Torquato <joao.otl@gmail.com>
2023-11-24 21:45:31 -0300

    Create home page, add bootstrap css and feather icons lib

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap", to: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js', preload: true
+pin "feather", to: 'https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.1/feather.min.js', preload: true
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :home, only: :index
+  root "home#index"
 end
